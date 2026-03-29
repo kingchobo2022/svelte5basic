a47df84da83b525f710fd8dcc99d9cb9baa871de
kingchobo <kingchobo2022@gmail.com>
2026-03-30 07:38:18 +0900

    2026-03-30 commit

---
+++ src/routes/ex22/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { setContext } from 'svelte';
+    import Child from './Child.svelte';
+
+    // 1. 공유할 반응형 상태 만들기
+    let theme = $state({
+        color: 'royalblue',
+        isDark: false
+    });
+
+    // 2. 컨텍스트에 이름표를 붙여서 저장하기
+    setContext('theme-context', theme); // 
+</script>
+
+<div>
+    <h1>부모 컴포넌트</h1>
+    <button onclick={() => theme.isDark = !theme.isDark}>테마 변경하기</button>
+
+    <Child />
+</div>
---
+++ src/routes/ex22/Child.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { getContext } from "svelte";
+
+    // 3. 부모가 붙인 이름표로 데이터 찾아오기
+    const theme = getContext('theme-context');
+</script>
+
+<div style="background-color: {theme.isDark ? '#333' : '#fff'}; 
+color: {theme.isDark ? '#fff' : '#333'}; padding: 10px;">
+
+    <h3>여기는 자식 컴포넌트입니다!</h3>
+    <p>현재 테마 색상 : {theme.color}</p>
+
+</div>
