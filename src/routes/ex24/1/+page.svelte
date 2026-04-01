245824ee456f8c5f416620bedb191ae9e7181fbd
kingchobo <kingchobo2022@gmail.com>
2026-04-02 07:44:29 +0900

    2026-04-22 commit

---
+++ src/routes/ex24/1/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  // 입력창을 제어하는 액션 함수
+  function uppercase(node) {
+    const handleInput = () => {
+      node.value = node.value.toUpperCase();
+    };
+
+    node.addEventListener('input', handleInput);
+
+    return {
+      destroy() {
+        node.removeEventListener('input', handleInput);
+      }
+    };
+  }
+
+  let username = $state("");
+</script>
+
+<label>
+  아이디(대문자 자동변환):
+  <input type="text" use:uppercase bind:value={username} />
+</label>
+<p>저장될 아이디: {username}</p>
---
+++ src/routes/ex24/2/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    let inputRef; // HTML 요소를 담을 변수
+
+    function handleSumit() {
+        const value = inputRef.value;
+        alert(`제출된 내용: ${value}`);
+        inputRef.value = ""; 
+        inputRef.focus(); 
+    }
+</script>
+
+<input type="text" bind:this={inputRef} placeholder="여기에 입력..." />
+
+<button onclick={handleSumit}>전송</button>
+
+
