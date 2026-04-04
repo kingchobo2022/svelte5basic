30983ecd7ec9a5e5debc40603cf4e2be36891365
kingchobo <kingchobo2022@gmail.com>
2026-04-04 10:24:18 +0900

    2026-04-04 commit

---
+++ src/routes/ex26/1/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let data = $state('로딩 중...');
+
+    // $effect는 컴포넌트가 화면에 완전히 나타난 뒤 실행됩니다.
+    $effect(() => {
+        console.log("컴포넌트가 마운트되었습니다!");
+
+        setTimeout(() => {
+            data = "서버에서 가져온 소중한 데이터";
+        }, 1500);
+    });
+</script>
+
+<h1>{data}</h1>
---
+++ src/routes/ex26/2/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    let seconds = $state(0);
+
+    $effect(() => {
+        const interval = setInterval(() => {
+            seconds++;
+            console.log("타이머 작동 중...", seconds);
+        }, 1000);
+
+        //[중요] $effect에서 함수를 리턴하면, 컴포넌트가 사라질 때 실행됩니다!
+        return () => {
+            console.log("컴포넌트가 제거되었습ㄴ다. 타이머를 멈춥니다.");
+            clearInterval(interval);
+        }
+    });
+</script>
+
+<div class="timer">
+    <h2>실행 시간 : {seconds}초</h2>
+</div>
