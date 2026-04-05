cfe4c2ecd9ee70de85a2ceb407d1c53195449e0b
kingchobo <kingchobo2022@gmail.com>
2026-04-05 12:30:50 +0900

    2026-04-05 commit

---
+++ src/routes/ex28/1/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    // 1. 데이터의 설계도를 만듭니다.
+    interface User {
+        name: string;
+        age: number;
+        isAdmin: boolean;
+    }
+
+    // 2. 변수 뒤에 :타입이름 을 붙여줍니다.
+    let user = $state<User>({
+        name: '왕초보',
+        age: 20,
+        isAdmin: false
+    });
+</script>
+
---
+++ src/routes/ex28/2/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    // Props의 타입을 정의합니다.
+    interface Props {
+        name: string;
+        score?: number; // ?를 붙이면 '없어도 됨' 이라는 뜻입니다.
+    }
+
+    let { name, score = 0 }: Props = $props();
+
+    name = "왕초보";
+    score = 20;
+    
+</script>
+
+<div>{name}님의 점수는 {score}점 입니다.</div>
