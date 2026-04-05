3e7fc44e3d603a2584c5650ebd23e25564e3a9a5
kingchobo <kingchobo2022@gmail.com>
2026-04-06 07:50:04 +0900

    2026-04-06 commit

---
+++ src/routes/blog/[id]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    // SvelteKit에서 현재 페이지 정보를 가져오는 도구
+    import { page } from '$app/state';
+
+    let blogId = $derived(page.params.id);
+</script>
+
+<h1>블로그 상세 페이지</h1>
+
+<p>현재 보고 계신 글 번호는 <strong>{blogId}</strong>번입니다.</p>
+
+<a href="/">목록으로 돌아가기</a>
---
+++ src/routes/shop/[category]/[item]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { page } from '$app/state';
+</script>
+
+<p>카테고리 : {page.params.category}</p>
+<p>상품명 : {page.params.item}</p>
